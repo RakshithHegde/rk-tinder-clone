ae76677e5f8c32a830b760de0c84baa176e1006c
Rakshith <rakshithhgd@gmail.com>
2021-11-14 18:22:45 +0530

    tinder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        {/* Header*/}
+        <Header />
+        <Router>
+          <Routes>
+            <Route path="/chat" element={<h2>Hi I am chat page</h2>} />
+
+            <Route path="/" element={<h2>Home page</h2>} />
+          </Routes>
+
+          {/*Tinder Cards */}
+          {/* Buttons below tinder cards*/}
+          {/* Chats screen*/}
+          {/* Individual chat screen*/}
+        </Router>
+      </header>
     </div>
   );
 }
