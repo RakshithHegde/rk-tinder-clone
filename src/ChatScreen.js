a2931af47280cec7752ecd835622cd6902909398
Rakshith <rakshithhgd@gmail.com>
2021-11-16 21:16:22 +0530

    tinder

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ChatScreen() {
-  return <div>Hello</div>;
+  const [messages, setMessages] = useState([
+    {
+      name: "ellen",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Sam_Rockwell_%2851492%29_%28cropped%29.jpg/1200px-Sam_Rockwell_%2851492%29_%28cropped%29.jpg",
+      message: "Whats up",
+    },
+    {
+      name: "ellen",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Sam_Rockwell_%2851492%29_%28cropped%29.jpg/1200px-Sam_Rockwell_%2851492%29_%28cropped%29.jpg",
+      message: "Whats up",
+    },
+    { message: "Whats up" },
+  ]);
+  return (
+    <div className="chatScreen">
+      <p>YOU MATCHED WITH ELLEN ON 10/11/21</p>
+    </div>
+  );
 }
 
 export default ChatScreen;
