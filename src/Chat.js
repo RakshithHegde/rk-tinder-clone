65ff5461c22a2f4db873458e57e8c6e9663e00bd
Rakshith <rakshithhgd@gmail.com>
2021-11-16 19:23:46 +0530

    tinder

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,18 @@
+import { Avatar } from "@material-ui/core";
+import "./chat.css";
 import React from "react";
 
-function Chat() {
-  return <div>hi</div>;
+function Chat({ name, message, imageUrl, timeStamp }) {
+  return (
+    <div className="chat">
+      <Avatar className="chat__image" alt={name} src={imageUrl} />
+      <div>
+        <h2>{name}</h2>
+        <p>{message}</p>
+      </div>
+      <p>{timeStamp}</p>
+    </div>
+  );
 }
 
 export default Chat;
