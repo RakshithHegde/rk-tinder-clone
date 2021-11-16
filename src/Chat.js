eb1b884abb02897778900c5a513ec53244e81890
Rakshith <rakshithhgd@gmail.com>
2021-11-16 22:04:59 +0530

    tinder

--- src/Chat.js
+++ src/Chat.js
@@ -1,17 +1,20 @@
 import { Avatar } from "@material-ui/core";
 import "./chat.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Chat({ name, message, imageUrl, timestamp }) {
   return (
-    <div className="chat">
-      <Avatar className="chat__image" alt={name} src={imageUrl} />
-      <div className="chat__details">
-        <h2>{name}</h2>
-        <p>{message}</p>
+    <Link to={`/chats/${name}`}>
+      <div className="chat">
+        <Avatar className="chat__image" alt={name} src={imageUrl} />
+        <div className="chat__details">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <p className="chat__timestamp">{timestamp}</p>
       </div>
-      <p className="chat__timestamp">{timestamp}</p>
-    </div>
+    </Link>
   );
 }
 
--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,24 +1,67 @@
+import { Avatar } from "@material-ui/core";
 import React, { useState } from "react";
+import "./ChatScreen.css";
 
 function ChatScreen() {
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([
     {
       name: "ellen",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Sam_Rockwell_%2851492%29_%28cropped%29.jpg/1200px-Sam_Rockwell_%2851492%29_%28cropped%29.jpg",
+        "https://admin.euro.savills.co.uk/_images/landscape-profile-images/btjones.jpg",
       message: "Whats up",
     },
     {
       name: "ellen",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Sam_Rockwell_%2851492%29_%28cropped%29.jpg/1200px-Sam_Rockwell_%2851492%29_%28cropped%29.jpg",
-      message: "Whats up",
+        "https://admin.euro.savills.co.uk/_images/landscape-profile-images/btjones.jpg",
+      message: "How are you",
     },
-    { message: "Whats up" },
+    { message: "Im fine wau" },
   ]);
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
   return (
     <div className="chatScreen">
-      <p>YOU MATCHED WITH ELLEN ON 10/11/21</p>
+      <p className="chatScreen__timeStamp">
+        YOU MATCHED WITH ELLEN ON 10/11/21
+      </p>
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chatScreen__message">
+            <p className="chatScreen__textUser">{message.message}</p>
+          </div>
+        )
+      )}
+
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          type="text"
+          placeholder="type a message..."
+        />
+        <button
+          onClick={handleSend}
+          type="submit"
+          className="chatScreen__inputButton"
+        >
+          Send
+        </button>
+      </form>
     </div>
   );
 }
